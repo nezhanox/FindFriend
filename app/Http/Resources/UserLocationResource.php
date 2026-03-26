2810caa35ca717ab15e1a619755e38038ee0803b
VladMaS <vladmaslyuk.1304@gmail.com>
2026-03-26 12:59:10 +0200

    fix: use @mixin for PHPStan type hints in JSON resources

--- app/Http/Resources/UserLocationResource.php
+++ app/Http/Resources/UserLocationResource.php
@@ -7,13 +7,14 @@ namespace App\Http\Resources;
 use App\Models\UserLocation;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin UserLocation
+ */
 class UserLocationResource extends JsonResource
 {
     /** @return array<string, mixed> */
     public function toArray($request): array
     {
-        /** @var UserLocation $this */
-
         return [
             'lat' => $this->lat,
             'lng' => $this->lng,
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,13 +7,14 @@ namespace App\Http\Resources;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin User
+ */
 class UserResource extends JsonResource
 {
     /** @return array<string, mixed> */
     public function toArray($request): array
     {
-        /** @var User $this */
-
         return [
             'name' => $this->name,
             'email' => $this->email,
@@ -23,7 +24,6 @@ class UserResource extends JsonResource
             'last_seen_at' => $this->last_seen_at,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-
         ];
     }
 }
